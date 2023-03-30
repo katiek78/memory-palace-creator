f8dcaa3e15e27eda5a39a85c10fd301f17ebabb0
katiek78 <kermodekatie@gmail.com>
2023-03-30 09:49:00 +0100

    feat: Add title, example map embed and config

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,2 +1,13 @@
-const Home : React.FC = () => <></>;
+import { API_KEY } from "../../config/config";
+
+const Home : React.FC = () => {
+
+    
+    return(
+        <>
+        <iframe width="600" height="450" style={{border:0}} loading="lazy" allowFullScreen
+src={`https://www.google.com/maps/embed/v1/streetview?location=53.3967,-2.3193&key=${API_KEY}`}></iframe>
+        </>
+    )
+}
 export default Home;
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,6 +2,7 @@ import Nav from "./Nav";
 
 const Header : React.FC = () => <>
     <header>
+        <h1>Memory Palace Creator</h1>
         <Nav />
     </header>
 </>;
