455e196391879801eb097449bc8ee8c6aa62794c
katiek78 <kermodekatie@gmail.com>
2023-04-11 14:50:03 +0100

    refactor: move all source code into src folder and add package.json

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+const path = require('path');
+require("dotenv").config({ path: path.resolve('config.env') });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
