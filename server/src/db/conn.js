ba44bc685d382486a401fd4ffa6a100e4471572c
katiek78 <kermodekatie@gmail.com>
2023-04-12 08:15:54 +0100

    feat: add error messaging in conn.js

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.NODE_ENV === "production" ? process.env.MONGODB_URI : process.env.ATLAS_URI;
-console.log(Db);
+if (!Db) {
+  throw new Error("Environment variable not set up");
+}
+
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,11 +23,11 @@ module.exports = {
             _db = db;
             console.log("Successfully connected to MongoDB.");
         } else {
-            console.log("Don't know when it might get here")
+            throw new Error("No DB");
         }
     } catch (error) {
         console.error('Failed to connect to MongoDB server');
-        throw error;
+        throw new Error("Failed to connect");
     }
 },
  
